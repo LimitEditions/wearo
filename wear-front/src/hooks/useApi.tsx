9968bfc4323bd3c52d2138177c9a6bda5491b147
AlexRax277 <rakovchen@list.ru>
2024-03-13 20:00:36 +0300

    разработан и проверен хук useApi, добавлено пару интерфейсов

--- wear-front/src/hooks/useApi.tsx
+++ wear-front/src/hooks/useApi.tsx
@@ -1,38 +1,52 @@
 import { useState, useEffect } from "react";
 import { Api } from '../api/Api.ts';
-// import { FullRequestParams } from "../api/http-client.ts";
+import { IApiResponse } from '../types/interfaces/ApiResponses/IApiResponse.ts';
 
-// const api = new Api({ baseURL: process.env.REACT_APP_BASE_URL});
 
-const useApi = (method: keyof Api, options: []): [any, boolean, any] => {
-    const [data, setData] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+const api = new Api({ baseURL: 'http://vne.su:8081' });
+
+const useApi = <T extends keyof Api, Data >(
+  method: T, 
+  params?: any, 
+  config?: any
+): [Data | null, boolean, any] => {
+    const [data, setData] = useState<Data | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<any>(null);
+
+    // Использование JSON.stringify для стабилизации объектов 
+    // Это необходимо для того, чтобы компонент не ререндерился бесконечное множество раз
+    const paramsString = JSON.stringify(params);
+    const configString = JSON.stringify(config);
 
     useEffect(() => {
-        let cancelled = false;
-
+        let isMounted = true;
         setIsLoading(true);
-        setData(null);
-        setError(null);
-        const response = async () => {
+        const fetchData = async () => {
             try {
-                // const result = await api[method](options)
-                // if (!cancelled) setData(result.data);
-            } catch(e: any) {
-                if (!cancelled) setError(e);
-            } finally {
-                if (!cancelled) setIsLoading(false);
+                const apiMethod = api[method] as unknown as (params: any, config?: any) => Promise<IApiResponse<Data>>;
+                // Парсим строки обратно в объекты для выполнения запроса
+                const result = await apiMethod(JSON.parse(paramsString), config ? JSON.parse(configString): undefined);
+                if (isMounted) {
+                    setData(result.data);
+                    setIsLoading(false);
+                }
+            } catch (e) {
+                if (isMounted) {
+                    setError(e);
+                    setIsLoading(false);
+                }
             }
-        }  
-        
-        response();
-
-        return () => {
-            cancelled = true;
         };
-// eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [method]);
+
+        fetchData();
+
+        // Очистка эффекта
+        return () => {
+            isMounted = false;
+        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [method, paramsString, configString]);
 
     return [data, isLoading, error];
 };
--- wear-front/src/utils/getStyles.ts
+++ wear-front/src/utils/getStyles.ts
@@ -1,4 +1,4 @@
-import { BlockStyle } from "../types/interfaces/Styles";
+import { BlockStyle } from "../types/interfaces/IStyles";
 
 export default function getStyles (styles: BlockStyle): string {
     const styleValues = Object.values(styles).filter(value => value !== undefined);
