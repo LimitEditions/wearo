63fac4284427f656f931ae9ae3445f9038f57a20
Marina <marina.nogovitsyna.98@mail.ru>
2024-03-13 12:00:13 +0300

    Сменилось расширение jedt.config

--- wear-front/jest.config.js
+++ wear-front/jest.config.js
@@ -3,9 +3,9 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+// import type {Config} from 'jest';
 
-const config: Config = {
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -78,16 +78,6 @@ const config: Config = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -201,4 +191,5 @@ const config: Config = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
 
-export default config;
+// export default config;
+module.exports = config;
