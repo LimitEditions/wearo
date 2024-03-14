b35e5892a3f21c3d5fc6137dc11e9c15ffcae21e
AlexRax277 <rakovchen@list.ru>
2024-03-14 23:18:06 +0300

    useApi-flag adding

--- wear-front/src/hooks/useApi.tsx
+++ wear-front/src/hooks/useApi.tsx
@@ -9,10 +9,11 @@ const api = new Api({ baseURL: 'http://vne.su:8081' });
 const useApi = <T extends keyof Api, Data >(
   method: T, 
   params?: any, 
-  config?: any
+  config?: any,
+  execute: boolean = false // Флаг выполннеия запроса, по умолчанию false
 ): [Data | null, boolean, any] => {
     const [data, setData] = useState<Data | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<any>(null);
 
     // Использование JSON.stringify для стабилизации объектов 
@@ -21,33 +22,35 @@ const useApi = <T extends keyof Api, Data >(
     const configString = JSON.stringify(config);
 
     useEffect(() => {
-        let isMounted = true;
-        setIsLoading(true);
-        const fetchData = async () => {
-            try {
-                const apiMethod = api[method] as unknown as (params: any, config?: any) => Promise<IApiResponse<Data>>;
-                // Парсим строки обратно в объекты для выполнения запроса
-                const result = await apiMethod(JSON.parse(paramsString), config ? JSON.parse(configString): undefined);
-                if (isMounted) {
-                    setData(result.data);
-                    setIsLoading(false);
+        if (execute) { //Проверяем флаг
+            let isMounted = true;
+            setIsLoading(true);
+            const fetchData = async () => {
+                try {
+                    const apiMethod = api[method] as unknown as (params: any, config?: any) => Promise<IApiResponse<Data>>;
+                    // Парсим строки обратно в объекты для выполнения запроса
+                    const result = await apiMethod(JSON.parse(paramsString), config ? JSON.parse(configString): undefined);
+                    if (isMounted) {
+                        setData(result.data);
+                        setIsLoading(false);
+                    }
+                } catch (e) {
+                    if (isMounted) {
+                        setError(e);
+                        setIsLoading(false);
+                    }
                 }
-            } catch (e) {
-                if (isMounted) {
-                    setError(e);
-                    setIsLoading(false);
-                }
-            }
-        };
+            };
 
-        fetchData();
+            fetchData();
 
-        // Очистка эффекта
-        return () => {
-            isMounted = false;
+            // Очистка эффекта
+            return () => {
+                isMounted = false;
+            };
         };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [method, paramsString, configString]);
+    }, [method, paramsString, configString, execute]);
 
     return [data, isLoading, error];
 };
