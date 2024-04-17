6e16653fab9a7d62acf0703c26e9a939145a7840
AlexRax277 <rakovchen@list.ru>
2024-04-17 18:40:00 +0300

    modal added

--- src/types/interfaces/componentsProps/IFormProps.ts
+++ src/types/interfaces/componentsProps/IFormProps.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { AuthModel, CreateUserModel, UserModel } from "../../../api/data-contracts";
 import { IAuthCreate } from "../ApiResponses/IAuthCreate";
 import { IApiError } from "../IApiError";
+import { NavigateFunction } from "react-router-dom";
 
 interface IFormProps<TUser, TData> {
     user: TUser,
@@ -11,6 +12,11 @@ interface IFormProps<TUser, TData> {
     data: TData,
     error: IApiError,
     isLoading: boolean,
+    modal: {
+        mod: boolean,
+        setMod: (event:boolean) => void,
+        navigate: NavigateFunction
+    }
 };
 
 export type ILoginFormProps = IFormProps<AuthModel, IAuthCreate>;
---
+++ src/types/interfaces/componentsProps/IModalProps.ts
@@ -0,0 +1,9 @@
+import { BlockStyle } from "../IStyles";
+
+export interface IModalProps {
+    isOpen: boolean,
+    setIsOpen: (event: boolean) => void;
+    title?: string,
+    children: React.ReactNode,
+    additionalStyles?: BlockStyle
+};
