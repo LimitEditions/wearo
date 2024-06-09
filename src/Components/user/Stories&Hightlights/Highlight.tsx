b1e0389db7f80fcdbe920845b61fea92e84e07c9
AlexRax277 <rakovchen@list.ru>
2024-06-10 01:10:40 +0300

    добавлена библиотека прогресс-бара и доработан компонент сторис

--- src/Components/user/Stories&Hightlights/Highlight.tsx
+++ src/Components/user/Stories&Hightlights/Highlight.tsx
@@ -8,8 +8,8 @@ export const Highlight = ({ highlight }: { highlight: HighlightModel}) => {
 
     return (
         <div>
-            <div className='flex flex-col items-center space-y-1 ' onClick={() => setOpen(true)}>
-                <img src={`${highlight.mainPhotoGuid}`} alt={highlight.name || ''} className='h-16 rounded-full border-2 border-black'/>
+            <div className='flex flex-col items-center space-y-1' onClick={() => setOpen(true)}>
+                <img src={`${highlight.mainPhotoGuid}`} alt={highlight.name || ''} className='h-14 w-14 object-contain rounded-full border-2 border-black'/>
                 <h3 className='text-xs'>{highlight.name}</h3>
             </div>
             <Stories highlightStories={highlight.stories || []} open={open} setOpen={setOpen} />
--- src/Components/user/Stories&Hightlights/Stories.tsx
+++ src/Components/user/Stories&Hightlights/Stories.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import { HighlightStoryModel } from '../../../api/data-contracts';
 import { Modal } from '../../common/Modal';
+import ProgressBar from '@ramonak/react-progress-bar';
 
 
 export const Stories = ({ highlightStories, open, setOpen }: 
@@ -13,70 +14,16 @@ export const Stories = ({ highlightStories, open, setOpen }:
         setOpen: React.Dispatch<React.SetStateAction<boolean>>
     }
 ) => {
-    const [loadingProgress, setLoadingProgress] = useState<number[]>([]);
-    const stories = useMemo(() => highlightStories.map(el => {
-        return el.story
-    }), [highlightStories]);
-    const intervalId = useRef<ReturnType<typeof setInterval> | null>(null);
+    // Извлечение историй из пришедших данных
+    const stories = useMemo(() => highlightStories.map(el => el.story), [highlightStories]);
+    const [paused, setPaused] = useState<boolean>(false); // Состояние паузы
+    const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0); // Индекс текущего слайда
+    const [loadingProgress, setLoadingProgress] = useState<number[]>(Array(stories.length).fill(0)); // Прогресс загрузки
+    const intervalId = useRef<ReturnType<typeof setInterval> | null>(null); // Идентификатор интервала
+    const lastSlideTimeout = useRef<ReturnType<typeof setTimeout> | null>(null); // Таймаут для последнего слайда
+    const sliderRef = useRef<Slider>(null); // Референс для слайдера
 
-    const settings = {
-        dots: false,
-        infinite: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        beforeChange: (oldIndex: number, newIndex: number) => {
-            setLoadingProgress(prev => {
-                const updated = [...prev];
-                updated[oldIndex] = 100;
-                return updated;
-            });
-        },
-        afterChange: (currentSlide: number) => {
-            if (intervalId.current) clearInterval(intervalId.current);
-            startLoading(currentSlide);
-            if (currentSlide === activeStories.length - 1) {
-                setTimeout(() => setOpen(false), 5000);
-            }
-        },
-    };
-
-    const startLoading = (currentSlide: number) => {
-        let progress = 0;
-        setLoadingProgress(prev => {
-            const updated = [...prev];
-            updated[currentSlide] = progress;
-            return updated;
-        });
-    
-        intervalId.current = setInterval(() => {
-            progress += 1;
-            setLoadingProgress(prev => {
-                const updated = [...prev];
-                updated[currentSlide] = progress;
-                return updated;
-            });
-            if (progress >= 100) {
-                if (intervalId.current) clearInterval(intervalId.current);
-            }
-        }, 50);
-    }
-
-    useEffect(() => {
-        if (open) {
-            setLoadingProgress(Array(stories.length).fill(0));
-            startLoading(0);
-        } else {
-            setLoadingProgress([]);
-            if (intervalId.current) clearInterval(intervalId.current);
-        }
-        return () => {
-            if (intervalId.current) clearInterval(intervalId.current);
-        };
-    }, [open, stories.length]);
-
+    // Проверка, активна ли история
     const isStoryActive = (createDT: string) => {
         const currentTime = new Date().getTime();
         const storyTime = new Date(createDT).getTime();
@@ -84,7 +31,91 @@ export const Stories = ({ highlightStories, open, setOpen }:
         return diff < 86400000;
     };
 
+    // Фильтрация активных историй
     const activeStories = stories.filter(story => isStoryActive(story?.createDT as string));
+
+    // Функция для начала загрузки прогресса
+    const startLoading = useCallback(() => {
+        let progress = 0;
+        setLoadingProgress(prev => {
+            const updated = [...prev];
+            updated[currentSlideIndex] = progress;
+            return updated;
+        });
+
+        intervalId.current = setInterval(() => {
+            if (!paused) {
+                progress += 1;
+                setLoadingProgress(prev => {
+                    const updated = [...prev];
+                    updated[currentSlideIndex] = progress;
+                    return updated;
+                });
+
+                // Если прогресс достиг 100%, переключаем на следующий слайд или закрываем модальное окно
+                if (progress >= 100) {
+                    if (intervalId.current) clearInterval(intervalId.current);
+                    if (currentSlideIndex < activeStories.length - 1) {
+                        sliderRef.current?.slickNext();
+                    } else if (currentSlideIndex === activeStories.length - 1) {
+                        lastSlideTimeout.current = setTimeout(() => {
+                            setOpen(false);
+                        }, 5000);
+                    }
+                }
+            }
+        }, 50);
+    }, [paused, setOpen, activeStories.length, currentSlideIndex]);
+
+    // Эффект для запуска загрузки прогресса при открытии модального окна
+    useEffect(() => {
+        if (open) {
+            setLoadingProgress(Array(activeStories.length).fill(0));
+            startLoading();
+        } else {
+            setLoadingProgress([]);
+            setCurrentSlideIndex(0);
+            if (intervalId.current) clearInterval(intervalId.current);
+            if (lastSlideTimeout.current) clearTimeout(lastSlideTimeout.current);
+        }
+
+        return () => {
+            if (intervalId.current) clearInterval(intervalId.current);
+            if (lastSlideTimeout.current) clearTimeout(lastSlideTimeout.current);
+        };
+    }, [open, activeStories.length, startLoading]);
+
+    // Настройки для слайдера
+    const settings = {
+        dots: false,
+        infinite: false,
+        autoplay: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        afterChange: (currentSlide: number) => {
+            setCurrentSlideIndex(currentSlide);
+            if (intervalId.current) clearInterval(intervalId.current);
+            if (lastSlideTimeout.current) clearTimeout(lastSlideTimeout.current);
+            if (!paused) {
+                startLoading();
+            }
+        },
+    };
+
+    // Обработчик клика для установки паузы
+    const handleClick = () => {
+        setPaused(!paused);
+        if (!paused) {
+            setLoadingProgress(prev => {
+                const updated = [...prev];
+                updated[currentSlideIndex] = 0;
+                return updated;
+            });
+        } else {
+            startLoading();
+        }
+    };
 
     return (
         <>
@@ -93,7 +124,7 @@ export const Stories = ({ highlightStories, open, setOpen }:
                 setIsOpen={setOpen} 
                 swipeable={true} 
                 additionalStyles={{
-                    container: 'fixed top-12 w-full px-2' ,
+                    container: 'fixed top-12 w-full px-2',
                     panel: 'p-0'
                 }}
             >
@@ -102,24 +133,30 @@ export const Stories = ({ highlightStories, open, setOpen }:
                         className="absolute right-5 top-5 text-white text-2xl leading-none z-50"
                         onClick={() => setOpen(false)}
                     >
-                        &times; 
+                        &times;
                     </button>
                     <div className="w-3/4 mx-auto h-1 bg-gray-300 flex space-x-1">
-                        {Array.from({ length: stories.length }).map((_, index) => (
-                            <div
-                                key={index}
-                                className="relative h-full bg-gray-900 flex-1"
-                            >
-                                <div 
-                                    className="absolute left-0 top-0 h-full bg-white transition-all duration-100"
-                                    style={{ width: `${loadingProgress[index]}%` }}
-                                ></div>
+                        {Array.from({ length: activeStories.length }).map((_, index) => (
+                            <div key={index} className="relative h-full bg-gray-900 flex-1">
+                                <ProgressBar 
+                                    completed={loadingProgress[index]} 
+                                    height="100%"
+                                    baseBgColor="#008000"
+                                    bgColor="#ff0000"
+                                    isLabelVisible={false}
+                                    transitionDuration="0.05s"
+                                    animateOnRender={true}
+                                />
                             </div>
                         ))}
                     </div>
-                    <Slider {...settings}>
-                        {activeStories.map((story) => (
-                            <div key={story?.guid} className="flex justify-center items-center w-full h-screen">
+                    <Slider {...settings} ref={sliderRef}>
+                        {activeStories.map((story, index) => (
+                            <div 
+                                key={story?.guid} 
+                                className="flex justify-center items-center w-full h-screen"
+                                onClick={handleClick}
+                            >
                                 <div className="relative w-full max-w-screen-sm" style={{ paddingTop: "177.78%" }}>
                                     <img src={`${story?.fileGuid}`} alt="Story" className="absolute top-1/2 left-0 w-full transform -translate-y-1/2 object-contain" />
                                 </div>
