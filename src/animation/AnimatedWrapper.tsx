ed7ff5b8351a37357bda0112447df14a506fcc3e
AlexRax277 <rakovchen@list.ru>
2024-07-07 21:46:07 +0300

    анимация только появление/изчезновение

--- src/animation/AnimatedWrapper.tsx
+++ src/animation/AnimatedWrapper.tsx
@@ -4,26 +4,26 @@ import { motion } from 'framer-motion';
 
 // виды анимации
 const pageVariants = [
-    {
-        initial: { opacity: 0, y: '-100%' },
-        in: { opacity: 1, y: 0 },
-        out: { opacity: 0, y: '100%' },
-    },
-    {
-        initial: { opacity: 0, y: '100%' },
-        in: { opacity: 1, y: 0 },
-        out: { opacity: 0, y: '-100%' },
-    },
-    {
-        initial: { opacity: 0, x: '100%' },
-        in: { opacity: 1, x: 0 },
-        out: { opacity: 0, x: '-100%' },
-    },
-    {
-        initial: { opacity: 0, x: '-100%' },
-        in: { opacity: 1, x: 0 },
-        out: { opacity: 0, x: '100%' },
-    },
+    // {
+    //     initial: { opacity: 0, y: '-100%' },
+    //     in: { opacity: 1, y: 0 },
+    //     out: { opacity: 0, y: '100%' },
+    // },
+    // {
+    //     initial: { opacity: 0, y: '100%' },
+    //     in: { opacity: 1, y: 0 },
+    //     out: { opacity: 0, y: '-100%' },
+    // },
+    // {
+    //     initial: { opacity: 0, x: '100%' },
+    //     in: { opacity: 1, x: 0 },
+    //     out: { opacity: 0, x: '-100%' },
+    // },
+    // {
+    //     initial: { opacity: 0, x: '-100%' },
+    //     in: { opacity: 1, x: 0 },
+    //     out: { opacity: 0, x: '100%' },
+    // },
     {
         initial: { opacity: 0 },
         in: { opacity: 1 },
