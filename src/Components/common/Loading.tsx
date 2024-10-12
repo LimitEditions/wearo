247dc6ba3569693bc65ecaef8772a960343a8870
Destrokhen <armagiddon19@gmail.com>
2024-10-12 13:37:14 +0300

    fix: убрал модуль и добавил анимацию в конфиг tailwind

--- src/Components/common/Loading.tsx
+++ src/Components/common/Loading.tsx
@@ -1,9 +1,7 @@
-import styles from './style.module.css';
-
 export function LoadingComponent() {
   return (
     <div className="absolute left-0 right-0 bottom-0 top-0 flex justify-center items-center bg-zinc-200/50">
-      <img className={styles.img} src="/images/wearo-logo.svg" alt="wear-logo"/>
+      <img className='animate-logo-loading' src="/images/wearo-logo.svg" alt="wear-logo"/>
     </div>
   )
 }
