f16b78d9770f1afcf561391adbc35e5926d2146e
janna1702 <jannaborsova@gmail.com>
2024-11-08 18:29:17 +0100

    slyles fix

--- src/Components/user/Brand.tsx
+++ src/Components/user/Brand.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-import { BrandModel } from '../../api/data-contracts';
-import useSubscribe from '../../hooks/useSubscribe';
-import { Link } from 'react-router-dom';
-import { Photo } from '../common/Photo';
-import { ContactButtons } from '../common/ContactButtons';
-import { Button } from '../common/Button';
-import { Highlights } from './Stories&Hightlights/Highlights';
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
-import { Arrow } from '../common/Arrow';
-import { Modal } from '../common/Modal';
-import { RingLoader } from 'react-spinners';
-import Item from '../common/ItemGroup/Item';
+import React, { useState } from "react";
+import { BrandModel } from "../../api/data-contracts";
+import useSubscribe from "../../hooks/useSubscribe";
+import { Link } from "react-router-dom";
+import { Photo } from "../common/Photo";
+import { ContactButtons } from "../common/ContactButtons";
+import { Button } from "../common/Button";
+import { Highlights } from "./Stories&Hightlights/Highlights";
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from "@headlessui/react";
+import { Arrow } from "../common/Arrow";
+import { Modal } from "../common/Modal";
+import { RingLoader } from "react-spinners";
+import Item from "../common/ItemGroup/Item";
+import { PiWhatsappLogo, PiTelegramLogo } from "react-icons/pi";
 
-
-export const Brand = ({ brandInfo }: { brandInfo: BrandModel}) => {
+export const Brand = ({ brandInfo }: { brandInfo: BrandModel }) => {
     // статус подписки с возможностью подписаться/отписаться
     const [subStatus, handlerSub] = useSubscribe(brandInfo.guid as string);
     const [modal, setModal] = useState<boolean>(false);
@@ -25,7 +29,7 @@ export const Brand = ({ brandInfo }: { brandInfo: BrandModel}) => {
         const timer = setTimeout(() => {
             setModal(false);
             event.target.disabled = false;
-        }, 5000)
+        }, 5000);
         return () => clearTimeout(timer);
     };
 
@@ -34,56 +38,87 @@ export const Brand = ({ brandInfo }: { brandInfo: BrandModel}) => {
     const handleRotate = () => {
         setIsRotated(!isRotated);
     };
-    
+
     return (
         <>
-            <Photo id={brandInfo?.photo || null} styles={'border-4'} alt={'фото бренда'}/>
-            <div className='flex justify-between '>
-                <Link to={`${brandInfo.link}`} target="_blank" rel="noopener noreferrer">{brandInfo?.name}</Link>
-                <div className='flex space-x-2'>
-                    <Button showButton={true} onClick={ handleClick }>
-                        { subStatus ? 'Вы подписаны': 'Подписаться'}
+            <Photo
+                id={brandInfo?.photo || null}
+                styles={"border-4"}
+                alt={"фото бренда"}
+            />
+            <div className="flex justify-between ">
+                <Link
+                    to={`${brandInfo.link}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {brandInfo?.name}
+                </Link>
+                <Button showButton={true} onClick={() => setModal(true)}>
+                    Поддержка
+                </Button>
+                <div className="flex space-x-2">
+                    <Button showButton={true} onClick={handleClick}>
+                        {subStatus ? "Вы подписаны" : "Подписаться"}
                     </Button>
                 </div>
             </div>
             <Highlights brandId={brandInfo.guid || null} />
-            <div className='text-center'>
-                {brandInfo?.description}
-            </div>
+            <div className="text-center">{brandInfo?.description}</div>
             <Disclosure>
-                <DisclosureButton className='w-full flex justify-between relative' onClick={handleRotate}>
-                    <div className='text-lg'>Коллекции</div>
-                    <div className={`absolute right-1 top-1/2 transform -translate-y-1/2 ${isRotated ? "rotate-90" : ""}`} >
-                        <Arrow direct={'right'} />
+                <DisclosureButton
+                    className="w-full flex justify-between relative"
+                    onClick={handleRotate}
+                >
+                    <div className="uppercase px-2">Коллекции</div>
+                    <div
+                        className={`absolute right-2 top-1/2 transform -translate-y-1/2 ${isRotated ? "rotate-90" : ""}`}
+                    >
+                        <Arrow direct={"right"} />
                     </div>
                 </DisclosureButton>
                 <DisclosurePanel>
-                    {brandInfo.collections?.map(col => {
-                        return <Item path={`../../collection/${col.guid}`} key={col.guid}>{col.name}</Item>
+                    {brandInfo.collections?.map((col) => {
+                        return (
+                            <Item
+                                path={`../../collection/${col.guid}`}
+                                key={col.guid}
+                            >
+                                {col.name}
+                            </Item>
+                        );
                     })}
                 </DisclosurePanel>
             </Disclosure>
             <Item path={`/products/${brandInfo.guid}`}>Изделия</Item>
-            <div>
-                Публикации
-            </div>
-            <div className='fixed bottom-24 w-full'>
-                <h3 className='text-center mb-2'>Служба поддержки</h3>
-                <ContactButtons 
-                    telegram={brandInfo.telegramId || ''} 
-                    whatsapp={brandInfo.whatsappId || ''} 
-                    email={brandInfo.email || ''}
-                    phone={'+78005558607'}
-                />
-            </div>
-            
+            <div className="uppercase px-2">Публикации</div>
+
             <Modal
                 isOpen={modal}
                 setIsOpen={setModal}
                 swipeable={false}
-                additionalStyles={{panel: 'w-full bg-transparent flex justify-center'}}
+                additionalStyles={{
+                    panel: "w-full bg-transparent flex justify-center",
+                }}
             >
-                <RingLoader color='#F9F8FF' loading={modal} size={45} speedMultiplier={0.75} />
+                <div className="fixed bottom-0 h-[400px] bg-white left-0 w-full">
+                    <h3 className="text-center mb-2">Служба поддержки</h3>
+                    <Item path={`https://t.me/${brandInfo.telegramId}`}>
+                        <Link
+                            to={`https://t.me/${brandInfo.telegramId}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <PiTelegramLogo className="text-gray-500 w-6 h-6 hover:text-gray-900 transition-all duration-300" />
+                        </Link>
+                    </Item>
+                    <ContactButtons
+                        telegram={brandInfo.telegramId || ""}
+                        whatsapp={brandInfo.whatsappId || ""}
+                        email={brandInfo.email || ""}
+                        phone={"+78005558607"}
+                    />
+                </div>
             </Modal>
         </>
     );
--- src/Components/user/Stories&Hightlights/Highlight.tsx
+++ src/Components/user/Stories&Hightlights/Highlight.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
-import { HighlightModel } from '../../../api/data-contracts'
-import { Stories } from './Stories'
+import React, { useState } from "react";
+import { HighlightModel } from "../../../api/data-contracts";
+import { Stories } from "./Stories";
 
-
-export const Highlight = ({ highlight }: { highlight: HighlightModel}) => {
+export const Highlight = ({ highlight }: { highlight: HighlightModel }) => {
     const [open, setOpen] = useState<boolean>(false);
 
     return (
         <div>
-            <div className='flex flex-col items-center space-y-1' onClick={() => setOpen(true)}>
-                <img src={`${highlight.mainPhotoGuid}`} alt={highlight.name || ''} className='h-14 w-14 object-contain rounded-full border-2 border-black'/>
-                <h3 className='text-xs'>{highlight.name}</h3>
+            <div
+                className="flex flex-col items-centerspace-y-1"
+                onClick={() => setOpen(true)}
+            >
+                <img
+                    src={`${highlight.mainPhotoGuid}`}
+                    alt={highlight.name || ""}
+                    className="h-[170px] w-[130px] object-contain rounded-xl border-[1.5px] border-lightgrey"
+                />
+                <h3 className="text-xs">{highlight.name}</h3>
             </div>
-            <Stories highlightStories={highlight.stories || []} open={open} setOpen={setOpen} />
+            <Stories
+                highlightStories={highlight.stories || []}
+                open={open}
+                setOpen={setOpen}
+            />
         </div>
     );
 };
